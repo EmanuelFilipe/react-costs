835cb815071d624a7fe084c19427b0f21456af15
efilipe <efilipe@magnasistemas.com.br>
2023-08-14 14:32:39 -0300

    Inserindo dados no banco via API

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,38 @@
 import ProjectForm from '../projects/ProjectForm'
+import { useNavigate } from 'react-router-dom'
 import styles from './NewProject.module.css'
 
 function NewProject() {
+
+    //permite fazer redirect nas paginas do sistemas
+    const history = useNavigate()
+
+    function createPost(project) {
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            // mandando dados para o servidor
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            //realiza um push no db.json/projects
+            history('/projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .catch((error) => console.error(error))
+    }
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto" />
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
     )
 }
