c9b7a5e486d5dded2e4adb96e035bcb263820ac9
efilipe <efilipe@magnasistemas.com.br>
2023-08-20 18:18:30 -0300

    método de exclusão e Página de edição de dados

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,6 +10,8 @@ import styles from './Projects.module.css'
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
+
     const location = useLocation()
     let message = ''
     
@@ -19,8 +21,8 @@ function Projects() {
             fetch("http://localhost:5000/projects", {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"
-            }
+                    "Content-Type": "application/json"
+                }
             })
             .then((resp) => resp.json())
             .then((data) => {
@@ -39,6 +41,23 @@ function Projects() {
         message = location.state.message
     }
     
+    function RemoveProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(resp => resp.json())
+        .then(() => {
+            //elimina o projeto pelo id
+            setProjects(projects.filter((p) => p.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(error => console.error(error))
+
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -46,6 +65,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 && 
                     projects.map((project) => (
@@ -55,6 +75,7 @@ function Projects() {
                          name={project.name}
                          budget={project.budget}
                          category={project?.category?.name}
+                         handleRemove={RemoveProject}
                         />
                     ))
                 }
